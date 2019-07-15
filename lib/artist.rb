f6a9e5919279c50822ea84699e79c7d7ad99e310
Chris Junker <elevatemedfit@gmail.com>
2019-07-15 12:10:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,9 @@
 class Artist
 
-  attr_accessor :name :artist
+  attr_accessor :name ,:artist
 
-  def initialize (name,artist)
+  def initialize (name)
     @name = name
-    @artist = artist
+    #@artist = artist
   end
 end
